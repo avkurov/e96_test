679914d83ad88b7fbdb3aca7efb985d1ec982edd
Andrey <drlibra@mail.ru>
2018-01-18 18:37:22 +0500

    - initial: added yii2 and codeception

---
+++ config/common.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'id' => 'e96-test',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'app\commands',
+];
---
+++ config/tests.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'id' => 'e96-test_tests',
+    'basePath' => dirname(__DIR__),
+];
